8fb00ca1d8d6fd40cc270ccc9f1e6d824de78820
Harish Kumar Reddy Kunchala <hkunchala@esri.com>
2024-12-02 13:43:45 -0800

    Now includes exception handling

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,14 +1,36 @@
 import 'package:dio/dio.dart';
 
+import '../models/tasks.dart';
+
 class ApiService {
   // Create an instance of Dio
   final _dio = Dio();
 
   // Method to fetch tasks from the API
-  Future<List<dynamic>> fetchTasks() async {
-    // Make a GET request to the API endpoint
-    final response = await _dio.get('https://jsonplaceholder.typicode.com/todos');
-    // Return the response data
-    return response.data;
+  Future<List<Task>> fetchTasks() async {
+    try {
+      // Make a GET request to the API endpoint
+      final response =
+          await _dio.get('https://jsonplaceholder.typicode.com/todos');
+      // Parse the response data into a list of Task objects
+      return (response.data as List)
+          .map((task) => Task.fromJSON(task))
+          .toList();
+    } on DioException catch (dioException) {
+      // Handle Dio Exceptions
+      if (dioException.type == DioExceptionType.connectionTimeout) {
+        throw Exception('Connection Timeout');
+      } else if (dioException.type == DioExceptionType.receiveTimeout) {
+        throw Exception('Receive Timeout');
+      } else if (dioException.type == DioExceptionType.badResponse) {
+        throw Exception(
+            'Received invalid status code: ${dioException.response?.statusCode}');
+      } else {
+        throw Exception('Something went wrong');
+      }
+    } catch (e) {
+      // Handle other errors
+      throw Exception('Failed to load tasks');
+    }
   }
 }
